c7c54ea37dbeb8ce4891f63eb697739b84ccb2aa
ddrightnow <ddrightnow@gmail.com>
2018-06-10 13:33:22 +0800

    8

--- practice.py
+++ practice.py
@@ -20,13 +20,25 @@ import re
 
 
 
-def factorial(n):
-    if n == 0:
-        return 1
+def is_power(a,b):
+    if(a%b != 0):
+        return False
+    elif(a/b == 1):
+        return True
     else:
-        recurse = factorial(n-1)
-        result = n * recurse
-        return result
+        return is_power(a/b,b)
+
+print(is_power(81,9))
+
+'''
+    elif a % b !=0 or b!=a**(1/i):
+        for i in range (5):
+            if a % b !=0 and b!=a**(1/i):
+                i=i+1
+                return True
+            else:
+                return False
     
-    
-factorial(5)
+print(is_power(81,9))
+#elif b = a**(1/(n+1))
+'''
