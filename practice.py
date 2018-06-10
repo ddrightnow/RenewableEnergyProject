2fcf92c7913e69e6bda848e63e8004ac058ec37f
ddrightnow <ddrightnow@gmail.com>
2018-06-10 13:21:21 +0800

    y

--- practice.py
+++ practice.py
@@ -20,28 +20,13 @@ import re
 
 
 
-def check_fermat(a,b,c,n):
-    if n > int(2) and (a**n + b**n == c**n):
-        print('Holy smokes, Fermat was wrong!')
+def factorial(n):
+    if n == 0:
+        return 1
     else:
-        print("No, that doesn’t work.")   
-
-def q():
-    a1 = np.random.randint(-10,10)
-    b1 = np.random.randint(astype(int))
-    c1 = np.random.random_integers
-    n1 = np.random.randint
-    a=a1
-    b=b1
-    c=c1
-    n=n1
-    #return check_fermat(a, b, c, n)
-    print (type(a1))
-    print (type(b))
-    print (type(c))
-    print (type(n))
+        recurse = factorial(n-1)
+        result = n * recurse
+        return result
     
-
-q()
-
-#check_fermat(124,7,89,2) 
+    
+factorial(5)
