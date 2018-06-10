a15988ed236ac9f13eeb01108aac270e90e214c3
ddrightnow <ddrightnow@gmail.com>
2018-06-10 13:20:15 +0800

    22

--- practice.py
+++ practice.py
@@ -20,6 +20,28 @@ import re
 
 
 
-x = np.random.randint
-a=x
-print (a)
+def check_fermat(a,b,c,n):
+    if n > int(2) and (a**n + b**n == c**n):
+        print('Holy smokes, Fermat was wrong!')
+    else:
+        print("No, that doesn’t work.")   
+
+def q():
+    a1 = np.random.randint(-10,10)
+    b1 = np.random.randint(astype(int))
+    c1 = np.random.random_integers
+    n1 = np.random.randint
+    a=a1
+    b=b1
+    c=c1
+    n=n1
+    #return check_fermat(a, b, c, n)
+    print (type(a1))
+    print (type(b))
+    print (type(c))
+    print (type(n))
+    
+
+q()
+
+#check_fermat(124,7,89,2) 
