8cf17ac476e4017afdd0fb73dd58d668deb96030
ddrightnow <ddrightnow@gmail.com>
2018-06-10 13:03:34 +0800

    5

--- practice.py
+++ practice.py
@@ -9,7 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import turtle
-#import time
+import time
 import random
 import os
 import dbm
@@ -20,36 +20,6 @@ import re
 
 
 
-def z():
-    x = np.random.randint(-10,10)
-    print (x)
-    
-    #reset_selective x
-    
-
-    #del x
-
-
-def q():
-    if x > 0:
-        print('x is positive')
-    elif x<0:
-        print ('x is negative')
-    else:
-        print ('x must be zero')
-
-#z()
-
-for i in range (25):
-    #x = np.random.randint(-10,10)
-    int x;
-    z()
-    q()
-    
-
-'''
-    for i in range(25):
-        x = np.random.randint(-10,10)
-    #z()
-        q()
-'''
+x = np.random.randint
+a=x
+print (a)
