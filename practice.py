0c9f12d142cfc446a8b2d8f850e6a0267f4d7a47
ddrightnow <ddrightnow@gmail.com>
2018-06-10 14:20:26 +0800

    32

--- practice.py
+++ practice.py
@@ -19,13 +19,42 @@ from collections import namedtuple
 import re
 
 
+def rotate_letter(letter, n):
+    """Rotates a letter by n places.  Does not change other chars.
 
-def find(word, letter,index):
-    #index = 0
-    while index < len(word):
-        if word[index] == letter:
-            return index
-        index = index + 1
-    return -1
+    letter: single-letter string
+    n: int
 
-print(find('mango','o',0))
+    Returns: single-letter string
+    """
+    if letter.isupper():
+        start = ord('A')
+    elif letter.islower():
+        start = ord('a')
+    else:
+        return letter
+
+    c = ord(letter) - start
+    i = (c + n) % 26 + start
+    return chr(i)
+
+
+def rotate_word(word, n):
+    """Rotates a word by n places.
+
+    word: string
+    n: integer
+
+    Returns: string
+    """
+    res = ''
+    for letter in word:
+        res += rotate_letter(letter, n)
+    return res
+
+
+if __name__ == '__main__':
+    print(rotate_word('cheer', 7))
+    print(rotate_word('melon', -10))
+    print(rotate_word('sleep', 9))
+    
