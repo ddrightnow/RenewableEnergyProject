dc50c331797ab333f92754e73154bb47abc9b3bf
ddrightnow <ddrightnow@gmail.com>
2018-06-10 12:54:50 +0800

    7

--- practice.py
+++ practice.py
@@ -4,7 +4,52 @@ Created on Fri Jun  8 00:06:12 2018
 
 @author: Dmob
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import turtle
+#import time
+import random
+import os
+import dbm
+import pickle
+import copy
+from collections import namedtuple
+import re
 
-n=23
 
-print (n% 3 ==0 or n%2==0)
+
+def z():
+    x = np.random.randint(-10,10)
+    print (x)
+    
+    #reset_selective x
+    
+
+    #del x
+
+
+def q():
+    if x > 0:
+        print('x is positive')
+    elif x<0:
+        print ('x is negative')
+    else:
+        print ('x must be zero')
+
+#z()
+
+for i in range (25):
+    #x = np.random.randint(-10,10)
+    int x;
+    z()
+    q()
+    
+
+'''
+    for i in range(25):
+        x = np.random.randint(-10,10)
+    #z()
+        q()
+'''
